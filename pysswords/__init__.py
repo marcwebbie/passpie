db03a4c0ad6c7602f519b66425997abb63149406
marcwebbie <marcwebbie@gmail.com>
2014-11-30 22:19:16 -0300

    Adding PysswordDB class

---
+++ pysswords/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+from .pysswords import (
+    PysswordDB
+)
---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from tempfile import NamedTemporaryFile
+import unittest
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import pysswords
+
+class PysswordsTests(unittest.TestCase):
+    def setUp(self):
+        self.tmp_db_file = NamedTemporaryFile(mode='w', delete=False)
+        self.tmp_db_file.close()
+
+    def tearDown(self):
+        os.remove(self.tmp_db_file.name)
+
+    def test_create_credential_database(self):
+        db_path = self.tmp_db_file.name
+
+        password = "=Sup3rh4rdp4ssw0rdt0cr4ck"
+        db = pysswords.PysswordDB(db_path=db_path, password=password)
+        self.assertIsNotNone(db)
+
+
+if __name__ == "__main__":
+    unittest.main()
