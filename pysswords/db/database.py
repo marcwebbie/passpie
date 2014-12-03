dcee4f71e2b17ee07074efea523ce38e039877c9
marcwebbie <marcwebbie@gmail.com>
2014-12-03 01:53:00 -0300

    Redesign database

--- pysswords/db/database.py
+++ pysswords/db/database.py
@@ -9,37 +9,45 @@ class Database(object):
 
     DEFAULT_CONTENT = '[{}]'
 
-    @staticmethod
-    def create(db_path, password, salt=None, iterations=100000):
-        """Create a new db file pointing to `db_path`
-        encrypted with `passwords`.
-        `salt` is the salt used. If none it generates a 8-byte salt.
-        `iterations` is the number of iterations of PBKDF2
-        """
-        # create default file
-        with open(db_path, "w") as default_file:
-            default_file.write(json.dumps([{}]))
+    def __init__(self, path, credentials, crypt_options):
+        self.path = path
+        self.credentials = credentials
+        self.crypt_options = crypt_options
 
-        with open(db_path, "rb") as f:
-            file_contents = f.read()
+    def create(path, crypt_options):
+        """Create a new empty Database at `path`
+        `crypt_options` is an instance of pysswords.crypt.CryptOptions
+
+        returns a Database instance
+        """
+
+        content = Database.DEFAULT_CONTENT
 
         aes_key, hmac_key, salt, iterations = crypt.make_keys(
-            password=password,
-            salt=salt,
-            iterations=iterations
+            password=crypt_options.password,
+            salt=crypt_options.salt,
+            iterations=crypt_options.iterations
         )
-        ciphertext, iv = crypt.encrypt(file_contents, aes_key)
+        ciphertext, iv = crypt.encrypt(content, aes_key)
         hmac = crypt.make_hmac(ciphertext, hmac_key)
 
         output = {
             "hmac": hmac,
-            "iterations": iterations
+            "iterations": crypt_options.iterations
         }
-        for key, value in ("ciphertext", ciphertext), ("iv", iv), ("salt", salt):
+
+        for key, value in (("ciphertext", ciphertext),
+                           ("iv", iv),
+                           ("salt", salt)):
             output[key] = base64.b64encode(value).decode("utf-8")
             output_data = json.dumps(output).encode("utf-8")
 
-        with open(db_path, "wb") as f:
+        with open(path, "wb") as f:
             f.write(output_data)
 
-        return db_path
+        database = Database(
+            path=path,
+            credentials=[],  # empty credentials
+            crypt_options=crypt_options
+        )
+        return database
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,13 @@
+import json
 import os
 import sys
 from tempfile import NamedTemporaryFile
 import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from pysswords.db import Database
+from pysswords.db import Database, Credential
+from pysswords.crypt import CryptOptions
+
 
 class PysswordsTests(unittest.TestCase):
     def setUp(self):
@@ -14,40 +17,43 @@ class PysswordsTests(unittest.TestCase):
         self.password = "=Sup3rh4rdp4ssw0rdt0cr4ck"
         self.default_iterations = 1000
 
-        self.db = Database.create(
-            db_path=self.db_path,
+        self.crypt_options = CryptOptions(
             password=self.password,
-            iterations=self.default_iterations
+            salt=None,
+            iterations=1000
+        )
+
+        self.db = Database.create(
+            path=self.db_path,
+            crypt_options=self.crypt_options
         )
 
     def tearDown(self):
         os.remove(self.db_path)
 
     def test_database_default_content(self):
-        import json
         expected_content = json.dumps([{}])
         self.assertEqual(Database.DEFAULT_CONTENT, expected_content)
 
     def test_create_credential_database(self):
         database = Database.create(
-            db_path=self.db_path,
-            password=self.password,
-            iterations=self.default_iterations
+            path=self.db_path,
+            crypt_options=self.crypt_options
         )
-        self.assertIsNotNone(database)
+        self.assertIsInstance(database, Database)
+        self.assertEqual(len(database.credentials), 0)
 
-    # def test_add_new_credential(self):
-    #     db_path = self.db_file.name
-
-    #     credential = db.Credential(
-    #         name="example",
-    #         login="john",
-    #         password="my-great-password",
-    #         login_url="http://example.org/login",
-    #         description="This is login credentials for example"
-    #     )
-    #     db.add_credential(db_path, credential)
-    #     self.assertEqual(db.count, 1)
+    def test_add_new_credential(self):
+        credential = Credential(
+            name="example",
+            login="john",
+            password="my-great-password",
+            login_url="http://example.org/login",
+            description="This is login credentials for example"
+        )
+        self.assertEqual(len(self.db.credentials), 0)
+        self.db.add_credential(credential)
+        self.assertEqual(len(self.db.credentials), 1)
 
 
 if __name__ == "__main__":
