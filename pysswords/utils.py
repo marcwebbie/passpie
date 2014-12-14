e6fa5fbd7a3a6694cc95a7dd39912e19db5f014f
marcwebbie <marcwebbie@gmail.com>
2014-12-14 20:42:53 -0300

    Add touch function into utils module

---
+++ pysswords/utils.py
@@ -0,0 +1,8 @@
+import os
+
+
+def touch(fname, mode=0o666, dir_fd=None, **kwargs):
+    flags = os.O_CREAT | os.O_APPEND
+    with os.fdopen(os.open(fname, flags=flags, mode=mode, dir_fd=dir_fd)) as f:
+        os.utime(f.fileno() if os.utime in os.supports_fd else fname,
+            dir_fd=None if os.supports_fd else dir_fd, **kwargs)
