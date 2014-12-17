d5e116bcf650bd91a69b24e1d11d3e05b738c002
marcwebbie <marcwebbie@gmail.com>
2014-12-16 22:36:37 -0300

    Remove duplicate version in setup

--- setup.py
+++ setup.py
@@ -37,6 +37,5 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.4',
     ],
 )
