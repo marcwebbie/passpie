88ac65884604d8fdaf639095b8e2553319d64203
marcwebbie <marcwebbie@gmail.com>
2015-01-10 21:04:59 -0200

    Add help to main options

--- pysswords/__main__.py
+++ pysswords/__main__.py
@@ -13,17 +13,25 @@ def parse_args(cli_args=None):
     parser = argparse.ArgumentParser(prog="Pysswords")
 
     group_db = parser.add_argument_group("Databse options")
-    group_db.add_argument("-I", "--init", action="store_true")
-    group_db.add_argument("-D", "--database", default=default_db())
-
+    group_db.add_argument("-I", "--init", action="store_true",
+                          help="create a new Pysswords database")
+    group_db.add_argument("-D", "--database", default=default_db(),
+                          help="specify path to database")
     group_cred = parser.add_argument_group("Credential options")
-    group_cred.add_argument("-a", "--add", action="store_true")
-    group_cred.add_argument("-g", "--get")
-    group_cred.add_argument("-u", "--update")
-    group_cred.add_argument("-r", "--remove")
-    group_cred.add_argument("-s", "--search")
-    group_cred.add_argument("-c", "--clipboard", action="store_true")
-    group_cred.add_argument("-P", "--show-password", action="store_true")
+    group_cred.add_argument("-a", "--add", action="store_true",
+                            help="add new credential")
+    group_cred.add_argument("-g", "--get",
+                            help="get credentials by name")
+    group_cred.add_argument("-u", "--update",
+                            help="update credentials")
+    group_cred.add_argument("-r", "--remove",
+                            help="remove credentials")
+    group_cred.add_argument("-s", "--search",
+                            help="search credentials. [regex supported]")
+    group_cred.add_argument("-c", "--clipboard", action="store_true",
+                            help="copy credential password to clipboard")
+    group_cred.add_argument("-P", "--show-password", action="store_true",
+                            help="show credentials passwords as plain text")
 
     args = parser.parse_args(cli_args)
     if args.clipboard and not args.get:
@@ -45,8 +53,7 @@ def main(cli_args=None):
     if args.add:
         try:
             interface.add_credential()
-        except ValueError as e:
-            # return logging.error(str(e))
+        except ValueError:
             return
     elif args.get:
         if args.clipboard:
