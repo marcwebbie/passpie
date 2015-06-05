7dd0a46ea4c6c4f1e4f1ce3ba7dbfee0c682a247
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-06-05 16:32:31 -0300

    refact(completion): use config_path in BASH template too

--- passpie/completion.py
+++ passpie/completion.py
@@ -10,7 +10,7 @@ function _passpie()
         local words=("${COMP_WORDS[@]}")
         unset words[0]
         unset words[$COMP_CWORD]
-        COMPREPLY=( $(compgen -W "$(grep -Ehrio '[A-Z0-9._%+-]+@[A-Z0-9.-]+(@[A-Z0-9_\-\.]+)?' $HOME/.passpie)" -- "$word") )
+        COMPREPLY=( $(compgen -W "$(grep -Ehrio '[A-Z0-9._%+-]+@[A-Z0-9.-]+(@[A-Z0-9_\-\.]+)?' {config_path})" -- "$word") )
     fi
 }
 complete -F _passpie 'passpie'
