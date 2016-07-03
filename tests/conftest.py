604b5b5152ab6ae472f7884b527fb3f57833a78f
marcwebbie <marcwebbie@gmail.com>
2016-07-02 22:13:32 -0300

    Test CLI init command

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,112 +1,15 @@
 from functools import partial
-from copy import deepcopy
-import tempfile
 
 import pytest
-
-from passpie.crypt import get_default_recipient, import_keys
-from passpie import config
-from . import helpers
-
-
-@pytest.fixture
-def mock_open():
-    try:
-        from mock import mock_open as mopen
-    except:
-        from unittest.mock import mock_open as mopen
-    return mopen()
-
-
-@pytest.fixture
-def mockie(mocker, mock_open):
-    mocker.mock_open = mock_open
-    return mocker
-
-
-@pytest.fixture
-def mock_config(mocker):
-    class Config(object):
-        def __init__(self, overrides={}):
-            self.overrides = overrides
-            self.values = config.DEFAULT.copy()
-            self.values['path'] = tempfile.NamedTemporaryFile().name
-            self.values.update(overrides)
-            mocker.patch.dict('passpie.validators.config.DEFAULT',
-                              self.values, clear=True)
-            mocker.patch('passpie.config.read', self.read)
-            mocker.patch('passpie.config.setup_crypt', self.setup_crypt)
-
-        def __enter__(self, *args, **kwargs):
-            return self
-
-        def __exit__(self, *args, **kwargs):
-            pass
-
-        def __getitem__(self, name):
-            return self.values[name]
-
-        def read(self, *args, **kwargs):
-            return self.overrides
-
-        def setup_crypt(self, *args, **kwargs):
-            keysfile = tempfile.NamedTemporaryFile(mode="w")
-            keysfile.write(helpers.KEYS)
-            configuration = self.values
-            configuration['homedir'] = tempfile.mkdtemp()
-            import_keys(keysfile.name, configuration['homedir'])
-            configuration['recipient'] = get_default_recipient(configuration['homedir'])
-            return configuration
-
-    return Config
-
-
-@pytest.fixture
-def creds(mocker, faker):
-    """Mock credentials to database"""
-    class CredentialFaker(object):
-        def make(self, number=1):
-            credentials = []
-            for _ in range(number):
-                credential = {
-                    'name': faker.domain_name(),
-                    'login': faker.user_name(),
-                    'password': faker.md5(),
-                    'comment': faker.word(),
-                }
-                credential['fullname'] =  u"{}@{}".format(
-                    credential['login'], credential['name'])
-                credentials.append(credential)
-            mocker.patch('passpie.cli.Database.credentials',
-                         return_value=deepcopy(credentials))
-            return credentials
-    return CredentialFaker()
-
-
-@pytest.fixture
-def runner(request, mocker):
-    """
-    Instance of `click.testing.CliRunner`. Can be configured with `@pytest.mark.runner_setup`
-    @pytest.mark.runner_setup(charset='cp1251')
-    def test_something(cli_runner):
-        ...
-    """
-    from click.testing import CliRunner
-    # mocker.patch('passpie.cli.create_keys', helpers.create_keys)
-    # mocker.patch('passpie.cli.ensure_dependencies')
-    # mocker.patch('passpie.database.Repository')
-    init_kwargs = {}
-    marker = request.node.get_marker('runner_setup')
-    if marker:
-        init_kwargs = marker.kwargs
-    return CliRunner(**init_kwargs)
+from click.testing import CliRunner
 
 
 @pytest.yield_fixture
-def irunner(mocker, runner):
+def irunner(mocker):
     """
     Instance of `click.testing.CliRunner` with automagically `isolated_filesystem()` called.
     """
+    runner = CliRunner()
     with runner.isolated_filesystem():
         runner.invoke = partial(runner.invoke, catch_exceptions=False)
         yield runner
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,228 +1,123 @@
-import csv
-
-import click
-from click.testing import CliRunner
-import pytest
-
-from passpie import cli
-from passpie.database import Database
-
-
-def test_ensure_passphrase_calls_decrypt_with_encrypted_data(mocker):
-    mock_logging = mocker.patch('passpie.cli.logging')
-    mock_encrypt = mocker.patch('passpie.cli.encrypt')
-    mock_decrypt = mocker.patch('passpie.cli.decrypt', return_value='OK')
-    config = {'recipient': 'recipient', 'homedir': 'homedir'}
-
-    cli.ensure_passphrase('passphrase', config=config)
-    assert mock_encrypt.called
-    assert mock_decrypt.called
-    mock_encrypt.assert_called_once_with('OK',
-                                         recipient=config['recipient'],
-                                         homedir=config['homedir'])
-    mock_decrypt.assert_called_once_with(mock_encrypt.return_value,
-                                         recipient=config['recipient'],
-                                         passphrase='passphrase',
-                                         homedir=config['homedir'])
-
-
-def test_ensure_passphrase_logs_error_when_decrypt_result_not_ok(mocker):
-    mock_logging = mocker.patch('passpie.cli.logging')
-    mock_encrypt = mocker.patch('passpie.cli.encrypt')
-    mock_decrypt = mocker.patch('passpie.cli.decrypt', return_value='NOT OK')
-    config = {'recipient': 'recipient', 'homedir': 'homedir'}
-    message_full = "Wrong passphrase for recipient: {} in homedir: {}".format(
-        config['recipient'],
-        config['homedir'],
-    )
-
-    with pytest.raises(click.ClickException):
-        cli.ensure_passphrase('passphrase', config=config)
-
-    assert mock_logging.error.called
-    mock_logging.error.assert_called_once_with(message_full)
-
-
-def test_call_to_cli_exit_with_error_when_missing_dependencies(mocker):
-    mocker.patch('passpie.cli.Database')
-    mocker.patch('passpie.cli.ensure_dependencies', side_effect=RuntimeError)
-    mocker.patch('passpie.cli.logging')
+"""
+def test_cat():
+    @click.command()
+    @click.argument('f', type=click.File())
+    def cat(f):
+        click.echo(f.read())
 
     runner = CliRunner()
-    result = runner.invoke(cli.cli)
-    assert result.exit_code != 0
+    with runner.isolated_filesystem():
+        with open('hello.txt', 'w') as f:
+            f.write('Hello World!')
 
-
-def test_cli_instantiate_database_with_configuration(mocker, mock_config, irunner):
-    mock_database = mocker.patch('passpie.cli.Database')
-    mocker.patch('passpie.cli.logging')
-
-    with mock_config() as configuration:
-        result = irunner.invoke(cli.cli)
-
-    output = result.output
-    assert mock_database.called
-    mock_database.assert_called_once_with(configuration.values)
-
-
-def test_cli_sets_logging_verbose_level_to_info_when_passing_one_v(mocker, mock_config, irunner):
-    mock_logging = mocker.patch('passpie.cli.logging')
-    configuration = {
-        'path': 'mocked',
-        'headers': ['name', 'login'],
-        'colors': {},
-        'table_format': 'plain',
-        'hidden': [],
-        'hidden_string': '*******',
-    }
-
-    with mock_config(configuration):
-        result = irunner.invoke(cli.cli, ['-v'])
-
-    assert mock_logging.basicConfig.called
-    _, kwargs = mock_logging.basicConfig.call_args
-    assert kwargs['level'] == mock_logging.INFO
-    assert result.exit_code == 0
-
-
-def test_cli_sets_logging_verbose_level_to_debug_when_passing_two_v(mocker, mock_config):
-    mock_logging = mocker.patch('passpie.cli.logging')
-
-    with mock_config():
-        runner = CliRunner()
-        result = runner.invoke(cli.cli, ['-vv'], catch_exceptions=False)
-
-    assert result.exit_code == 0
-    assert mock_logging.basicConfig.called
-    _, kwargs = mock_logging.basicConfig.call_args
-    assert kwargs['level'] == mock_logging.DEBUG
-
-
-def test_cli_sets_logging_verbose_level_to_critical_when_no_verbose_passed(mocker, mock_config):
-    mock_logging = mocker.patch('passpie.cli.logging')
-
-    with mock_config():
-        runner = CliRunner()
-        result = runner.invoke(cli.cli, [], catch_exceptions=False)
-
-    assert result.exit_code == 0
-    assert mock_logging.basicConfig.called
-    _, kwargs = mock_logging.basicConfig.call_args
-    assert kwargs['level'] == mock_logging.CRITICAL
-
-
-
-def test_validate_cols_returns_dict_with_col_position(mocker):
-    value = ',name,login,,password,,,comment'
-    cols = {
-        'name': 1,
-        'login': 2,
-        'password': 4,
-        'comment': 7,
-    }
-    result = cli.validate_cols(ctx='ctx', param='param', value=value)
-    assert result == cols
-
-
-def test_validate_cols_returns_none_when_missing_cols(mocker):
-    value_missing_name = ',login,,password,,,comment'
-    value_missing_login = ',name,,password,,,comment'
-    value_missing_password = ',name,login,,,,comment'
-    value_missing_comment = ',name,login,,password,,,'
-
-    with pytest.raises(click.BadParameter):
-        cli.validate_cols(ctx='', param='', value=value_missing_name)
-    with pytest.raises(click.BadParameter):
-        cli.validate_cols(ctx='', param='', value=value_missing_login)
-    with pytest.raises(click.BadParameter):
-        cli.validate_cols(ctx='', param='', value=value_missing_password)
-
-    assert cli.validate_cols(ctx='', param='', value=value_missing_comment) is not None
-
-
-class CliTests(object):
-
-    def test_cli_cli_list_credentials(self, mockie, mock_config, creds, irunner):
-        credentials = creds.make(5)
-
-        result = irunner.invoke(cli.cli, [])
-        output = result.output
+        result = runner.invoke(cat, ['hello.txt'])
         assert result.exit_code == 0
-        for credential in credentials:
-            assert credential['name'] in output, output
-            assert credential['login'] in output
-            assert credential['comment'] in output
+        assert result.output == 'Hello World!\n'
+"""
+import os
+import tarfile
+import tempfile
+
+import click
+import yaml
+
+from passpie.cli import cli
 
 
-class CliAddTests(object):
+def test_cli_init_creates_passpie_db_file(irunner):
+    """passpie --passphrase p init
+    """
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, ["--passphrase", "p", "init"])
+        assert "passpie.db" in os.listdir(os.curdir)
+        assert result.exit_code == 0
 
-    def test_add_credentials_with_random_password(self, mocker, mock_config, irunner):
-        fullname = "foo@example.com"
-        mock_genpass = mocker.patch('passpie.cli.genpass', return_value='random')
 
-        with mock_config() as cfg:
-            pattern = cfg['genpass_pattern']
-            result = irunner.invoke(cli.cli, ['add', fullname, '--random'])
+def test_cli_init_creates_passpie_db_file_with_expected_files(irunner):
+    """passpie --passphrase p init
+    """
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, ["--passphrase", "p", "init"])
+        tempdir = tempfile.mkdtemp()
+        with tarfile.open("passpie.db") as tf:
+            tf.extractall(tempdir)
 
-            assert result.exit_code == 0
-            assert mock_genpass.called is True
-            mock_genpass.assert_called_once_with(pattern=pattern)
+        assert ".passpie" in os.listdir(tempdir)
+        assert "config.yml" in os.listdir(tempdir)
+        assert "keys.yml" in os.listdir(tempdir)
+        with open(os.path.join(tempdir, "config.yml")) as f:
+            config_dict = yaml.load(f.read())
+        with open(os.path.join(tempdir, "keys.yml")) as f:
+            keys_dict = yaml.load(f.read())
+        assert "PUBLIC" in keys_dict
+        assert "PRIVATE" in keys_dict
 
-    def test_add_credentials_with_force_rewrites_credential(self, mocker, creds, mock_config, irunner):
-        credentials = creds.make(2)
-        fullname = credentials[0]['fullname']
-        mock_genpass = mocker.patch('passpie.cli.genpass', return_value='random')
-        mock_db_add = mocker.patch('passpie.cli.Database.add')
-        mocker.patch('passpie.cli.Database.credential',
-                     return_value=credentials[0])
 
-        with mock_config() as cfg:
-            pattern = cfg['genpass_pattern']
-            result = irunner.invoke(cli.cli, ['add', fullname, '--random', '--force'])
+def test_cli_init_raise_error_when_passpie_db_exists(irunner):
+    """passpie --passphrase p init
+    """
+    with irunner.isolated_filesystem():
+        with open("passpie.db", "w"):
+            pass
+        result = irunner.invoke(cli, ["--passphrase", "p", "init"])
+        assert result.exit_code == 1
+        assert result.output == u"Error: Path 'passpie.db' exists [--force] to overwrite\n"
 
-            assert result.exit_code == 0
-            assert mock_db_add.called is True
 
-    def test_add_credentials_with_copy_copy_to_clipboard(self, mocker, mock_config, irunner):
-        mock_genpass = mocker.patch('passpie.cli.genpass', return_value='random')
-        mock_copy = mocker.patch('passpie.cli.clipboard.copy')
+def test_cli_init_overrides_database_when_passpie_db_exists_and_force_is_passed(irunner):
+    """passpie --passphrase p init --force
+    """
+    with irunner.isolated_filesystem():
+        with open("passpie.db", "w"):
+            pass
+        result = irunner.invoke(cli, ["--passphrase", "p", "init", "--force"])
+        assert result.exit_code == 0
 
-        with mock_config() as cfg:
-            pattern = cfg['genpass_pattern']
-            result = irunner.invoke(cli.cli, ['add', "fullname@name", '--random', '--copy'])
 
-            assert result.exit_code == 0
-            assert mock_copy.called is True
+def test_cli_init_add_recipient_to_config_if_is_passed(irunner):
+    """passpie --passphrase p init --recipient jdoe@example.com
+    """
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, [
+            "--passphrase", "p", "init", "--recipient", "jdoe@example.com"])
+        assert result.exit_code == 0
+        tempdir = tempfile.mkdtemp()
+        with tarfile.open("passpie.db") as tf:
+            tf.extractall(tempdir)
+        with open(os.path.join(tempdir, "config.yml")) as f:
+            config_dict = yaml.load(f.read())
+        assert config_dict["recipient"] == "jdoe@example.com"
 
-    def test_add_credentials_with_interactive_open_cred_in_editor(self, mocker, mock_config, irunner):
-        filename = 'path/to/credential.pass'
-        mocker.patch('passpie.cli.Database.filename', return_value=filename)
-        mock_genpass = mocker.patch('passpie.cli.genpass', return_value='random')
-        mock_click_edit = mocker.patch('passpie.cli.click.edit')
 
-        with mock_config() as cfg:
-            pattern = cfg['genpass_pattern']
-            result = irunner.invoke(cli.cli, ['add', "fullname@name", '--random', '--interactive'])
+def test_cli_init_dont_create_git_repo_if_no_git_is_passed(irunner):
+    """passpie --passphrase p init --no-git
+    """
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, ["--passphrase", "p", "init", "--no-git"])
+        assert result.exit_code == 0, result.output
+        with tarfile.open("passpie.db") as tf:
+            assert "./.git" not in tf.getnames()
 
-            assert result.exit_code == 0
-            assert mock_click_edit.called is True
-            mock_click_edit.assert_called_once_with(filename=filename)
 
-class CliUpdateTests(object):
+def test_cli_init_dont_create_git_repo_when_config_git_is_false(irunner, mocker):
+    """passpie --passphrase p init
+    """
+    mocker.patch("passpie.cli.config_load", return_value={"GIT": False})
 
-    def test_update_credentials_with_interactive_open_cred_in_editor(self, mocker, creds, mock_config, irunner):
-        credentials = creds.make(2)
-        fullname = credentials[0]['fullname']
-        filename = 'path/to/credential.pass'
-        mocker.patch('passpie.cli.Database.credential', return_value=credentials[0])
-        mocker.patch('passpie.cli.Database.filename', return_value=filename)
-        mock_genpass = mocker.patch('passpie.cli.genpass', return_value='random')
-        mock_click_edit = mocker.patch('passpie.cli.click.edit')
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, ["--passphrase", "p", "init"])
+        assert result.exit_code == 0, result.output
+        with tarfile.open("passpie.db") as tf:
+            assert "./.git" not in tf.getnames()
 
-        with mock_config() as cfg:
-            pattern = cfg['genpass_pattern']
-            result = irunner.invoke(cli.cli, ['update', fullname, '--random', '--interactive'])
 
-            assert result.exit_code == 0
-            assert mock_click_edit.called is True
-            mock_click_edit.assert_called_once_with(filename=filename)
+def test_cli_init_create_git_repo_when_config_git_is_true(irunner, mocker):
+    """passpie --passphrase p init
+    """
+    mocker.patch("passpie.cli.config_load", return_value={"GIT": True})
+
+    with irunner.isolated_filesystem():
+        result = irunner.invoke(cli, ["--passphrase", "p", "init"])
+        assert result.exit_code == 0, result.output
+        with tarfile.open("passpie.db") as tf:
+            assert "./.git" in tf.getnames()
