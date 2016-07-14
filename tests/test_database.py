b5d0934839dcb1ed0cd9d703f6ed34a3dba99cdf
marcwebbie <marcwebbie@gmail.com>
2016-07-14 02:15:27 -0300

    Fix database errors

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,11 @@
 import pytest
 
-from passpie.database import split_fullname, make_fullname, Database
+from passpie.database import (
+    split_fullname,
+    make_fullname,
+    Database,
+    CredentialFactory,
+)
 
 
 def test_split_fullname_raises_value_error_when_invalid_name(mocker):
@@ -24,3 +29,23 @@ def test_make_fullname_returns_expected_fullname(mocker):
     assert make_fullname("_", "bar") == "_@bar"
     assert make_fullname(None, "bar") == "@bar"
     assert make_fullname("", "bar") == "@bar"
+
+
+def test_credential_factory_returns_expected_dictionary(mocker):
+    credential = CredentialFactory()
+    assert credential.get("login") is not None
+    assert credential.get("name") is not None
+    assert credential.get("password") is not None
+    assert credential.get("comment") is not None
+
+
+def test_credential_factory_splits_fullname_into_login_name(mocker):
+    credential = CredentialFactory(fullname="foo@bar")
+    assert credential["login"] == "foo"
+    assert credential["name"] == "bar"
+
+
+def test_credential_sets_fake_password(mocker):
+    mock_faker = mocker.patch("passpie.database.Faker")
+    credential = CredentialFactory(fullname="foo@bar")
+    assert credential["password"] == mock_faker().password()
