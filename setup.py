fa9429cbed5e1b0b2422f71066aa8e230bae6212
marcwebbie <marcwebbie@gmail.com>
2016-07-16 11:12:28 -0300

    Bump version: 1.5.3 → 1.5.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     from distutils.core import setup, Command, find_packages
 
 
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 
 with io.open('README.rst', encoding='utf-8') as readme_file:
     long_description = readme_file.read() + "\n\n"
