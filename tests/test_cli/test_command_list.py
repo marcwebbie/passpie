8b74153fc59cf3ada971c8e22d462cc43f76d9ef
marcwebbie <marcwebbie@gmail.com>
2017-03-15 19:20:48 -0300

    Fix test cli list command

--- tests/test_cli/test_command_list.py
+++ tests/test_cli/test_command_list.py
@@ -2,17 +2,22 @@ from passpie.cli import cli
 
 
 def test_cli_list_all_credentials(irunner, mocker):
-    irunner.run(cli, "add --random foo@bar spam@egg")
-    result = irunner.run(cli, "list")
+    irunner.passpie("add --random foo@bar spam@egg")
+    irunner.passpie("add --random spam@egg spam@egg")
+    result = irunner.passpie("list")
+
     assert result.exit_code == 0
-    assert result.exit_code == 0, result.output
     assert "foo" in result.output
+    assert "bar" in result.output
     assert "spam" in result.output
+    assert "egg" in result.output
 
 
 def test_cli_list_filter_credentials(irunner, mocker):
-    irunner.run(cli, "add --random foo@bar spam@egg")
-    result = irunner.run(cli, "list foo")
+    irunner.passpie("add --random foo@bar spam@egg")
+    irunner.passpie("add --random spam@egg spam@egg")
+    result = irunner.passpie("list foo")
+
     assert result.exit_code == 0, result.output
     assert "foo" in result.output
     assert "spam" not in result.output
