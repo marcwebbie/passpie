9723085689b146f310deabd3d01e279f71e98292
er <emanuel.reisinger@cargonet.software>
2024-03-21 16:42:32 +0100

    Versions set to 1.6.2

--- passpie/cli.py
+++ passpie/cli.py
@@ -17,7 +17,7 @@ from .history import clone
 from .validators import validate_config, validate_cols, validate_remote
 
 
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 pass_db = click.make_pass_decorator(Database, ensure=False)
 logging.basicConfig(format="[%(levelname)s:passpie.%(module)s]: %(message)s")
 
